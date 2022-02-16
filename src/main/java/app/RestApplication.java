e8cefcacd27009b65f359d230bf1810c1f2520dd
Sarahhabbi <habbisarah.1@gmail.com>
2022-02-16 11:42:28 +0100

    init web app

---
+++ src/main/java/app/RestApplication.java
@@ -0,0 +1,28 @@
+package app;
+
+import exception.RuntimeExceptionMapper;
+import filter.GsonProvider;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> sets = new HashSet<>(1);
+        sets.add(new TestEndpoint());
+        return sets;
+    }
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> sets = new HashSet<>(1);
+        sets.add(GsonProvider.class);
+        sets.add(RuntimeExceptionMapper.class);
+        return sets;
+    }
+
+}
