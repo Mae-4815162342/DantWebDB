8bef48636ee23a7d2ecb5906d127082b62d74b6a
Sarahhabbi <habbisarah.1@gmail.com>
2022-02-16 11:52:59 +0100

    init web app

---
+++ src/main/java/app/TestEndpoint.java
@@ -0,0 +1,25 @@
+package app;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/api/test")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TestEndpoint {
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GET
+    @Path("/exception")
+    public Response exception() {
+        throw new RuntimeException("Mon erreur");
+    }
+
+}
