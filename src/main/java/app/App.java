58b6a3013cdf785aa0768ad2ff09d2995b2834ea
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-03-09 12:12:18 +0100

    Add code to the store data and query it

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,16 +6,19 @@ import filter.GsonProvider;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @ApplicationPath("")
 public class App extends Application {
-
+    public static List<Map<String, String>> data;
     @Override
     public Set<Object> getSingletons() {
         Set<Object> sets = new HashSet<>(1);
         sets.add(new TestEndpoint());
         sets.add(new Create());
+        sets.add(new Table());
         return sets;
     }
     @Override
--- src/main/java/app/Create.java
+++ src/main/java/app/Create.java
@@ -5,32 +5,42 @@ import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/api/create")
-@Produces(MediaType.APPLICATION_JSON)
 public class Create {
+    public List<Map<String, String>> parseCSV(InputPart inputPart) throws IOException{
+        List<Map<String, String>> res = new ArrayList<Map<String, String>>();
+        InputStream inputStream = inputPart.getBody(InputStream.class, null);
+        byte[] bytes = IOUtils.toByteArray(inputStream);
+        String fic = new String(bytes);
+        fic = fic.replaceAll("\\r", "");
+        String[] lines = fic.split("\n");
+        String[] headers = lines[0].split(",");
+        for(int i = 1; i<lines.length -1; i++){
+            Map<String, String> line = new HashMap<String, String>();
+            String[] content = lines[i].split(",");
+            line.put(headers[0], content[0]);
+            line.put(headers[1], content[1]);
+            res.add(line);
+        }
+        return res;
+    } 
     @POST
     @Consumes("multipart/form-data")
-    public Response post(MultipartFormDataInput input) {
+    public Response post(MultipartFormDataInput input) throws IOException {
         final String UPLOADED_FILE_PARAMETER_NAME = "file";
         
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
         List<InputPart> inputParts = uploadForm.get(UPLOADED_FILE_PARAMETER_NAME);
         for (InputPart inputPart : inputParts) {
-            try {
-                InputStream inputStream = inputPart.getBody(InputStream.class, null);
-                byte[] bytes = IOUtils.toByteArray(inputStream);
-                String line = new String(bytes);
-                System.out.println(line);
-            } catch (IOException e) {
-                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-            }
+            App.data = parseCSV(inputPart);
         }
         return Response.ok("Bien reçu").build();
     }
