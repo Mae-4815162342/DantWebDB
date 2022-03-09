48b8a0318a220b8f2b37db938c18410536002f91
Mae-4815162342 <maelysdelouis@gmail.com>
2022-03-09 17:05:02 +0100

    create table in simple structure

--- src/main/java/app/CreateTable.java
+++ src/main/java/app/CreateTable.java
@@ -1,5 +1,8 @@
 package app;
 
+import model.ClientTable;
+import model.Structure;
+import model.Tuple;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
@@ -23,14 +26,19 @@ public class CreateTable {
     public Response createTable(MultipartFormDataInput input) throws IOException {
         final String TABLE_NAME = "name";
         final String NUMBER_OF_COLUMNS= "columns";
-        ArrayList<String> res = new ArrayList<>();
-        ArrayList<String> columns = new ArrayList<>();
+        ArrayList<Object> res = new ArrayList<>();
+        ArrayList<Tuple> columns = new ArrayList<>();
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
 
         List<InputPart> tableNameInput = uploadForm.get(TABLE_NAME);
         InputStream nameInputStream = tableNameInput.get(0).getBody(InputStream.class, null);
         String tableName = new String(IOUtils.toByteArray(nameInputStream)).toUpperCase();
         res.add("name : " + tableName);
+
+        ClientTable oldTable = Structure.getTableByName(tableName);
+        if(oldTable != null) {
+            return Response.status(400).entity("This table(" + tableName + ") already exist!\n" + oldTable.toString()).type("plain/text").build();
+        }
 
         List<InputPart> colNumberInput = uploadForm.get(NUMBER_OF_COLUMNS);
         InputStream colNumberInputStream = colNumberInput.get(0).getBody(InputStream.class, null);
@@ -44,12 +52,13 @@ public class CreateTable {
                 InputStream columnInputStream = columnInput.get(0).getBody(InputStream.class, null);
                 String column = new String(IOUtils.toByteArray(columnInputStream));
                 String[] columnParameters = column.split(";");
-                columns.add(columnParameters[0].toUpperCase() + " (Type: " + columnParameters[1] + ")");
+                columns.add(new Tuple(columnParameters[0].toUpperCase(),columnParameters[1]));
             }
+            res.add("columns : " + columns.toString());
+            Structure.createTable(tableName, columns);
         } else {
             return Response.status(400).entity("Bad number of columns!").type("plain/text").build();
         }
-
-        return Response.ok("Table created\n" + res + "\n" + columns).build();
+        return Response.ok("Table created!\n" + Structure.showTables()).build();
     }
 }
