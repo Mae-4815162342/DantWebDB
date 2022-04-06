c64dacbb2a2c161d91b8657bb393f3a2cac7fe38
Mae-4815162342 <maelysdelouis@gmail.com>
2022-04-06 15:07:46 +0200

    insertEndePoint

--- src/main/java/model/ClientTable.java
+++ src/main/java/model/ClientTable.java
@@ -1,2 +1,40 @@
-package model;public class ClientTable {
+package model;
+
+import java.util.ArrayList;
+
+public class ClientTable {
+    public String name;
+    public ArrayList<String> columnsNames = new ArrayList<>();
+    public ArrayList<String> columnsTypes = new ArrayList<>();
+    public ArrayList<String> data = new ArrayList<>();
+
+    public ClientTable(String name, ArrayList<Tuple> columns) {
+        this.name = name;
+        for(Tuple column: columns) {
+            columnsNames.add((String)column.first);
+            columnsTypes.add((String)column.second);
+        }
+    }
+
+    public String toString() {
+        String res = "table " + name + "\n{";
+        for(int i = 0; i < columnsNames.size(); i++) {
+            res += "\t" + columnsNames.get(i) + "(" + columnsTypes.get(i) + ")\n";
+        }
+        res+="}";
+        return res;
+    }
+
+    public void insertLine(String line) {
+        data.add(line);
+    }
+
+    public String getData() {
+        String res = name + " data :\n{\n";
+        for(String line: data) {
+            res+= "\t" + line + "\n";
+        }
+        res+="}";
+        return res;
+    }
 }
--- src/main/java/model/Tuple.java
+++ src/main/java/model/Tuple.java
@@ -1,2 +1,15 @@
-package model;public class Tuple {
+package model;
+
+public class Tuple {
+    public Object first;
+    public Object second;
+
+    public Tuple(Object f, Object s) {
+        first = f;
+        second = s;
+    }
+
+    public String toString() {
+        return "{" + first + "," + second + "}";
+    }
 }
