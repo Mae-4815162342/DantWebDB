967a8f4df529f2fe7581af26fa376b335fc512ad
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-04-06 16:16:33 +0200

    Can store chunks inside buffers,3min for ingestion

--- src/main/java/app/Create.java
+++ src/main/java/app/Create.java
@@ -11,6 +11,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,10 +21,27 @@ import javax.ws.rs.core.Response;
 @Path("/api/create")
 public class Create {
     final int CHUNK_SIZE = 100_000; 
+    public int  sumSizeBuffer(List<ByteArrayOutputStream> buffers){
+        int res = 0;
+        for(ByteArrayOutputStream buffer : buffers){
+            res += buffer.size();
+        }
+        return res;
+    }
+    public void sendChunks(List<ByteArrayOutputStream> buffers){
+        //envoi de requête
+        System.out.println("ON VIDE");
+        for(ByteArrayOutputStream buffer : buffers){
+            buffer.reset();
+        }
+    }
     public byte[] parseCSV(InputPart inputPart) throws IOException{
         InputStream inputStream = inputPart.getBody(InputStream.class, null);
         BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream));
-        
+        List<ByteArrayOutputStream> buffers = new ArrayList<ByteArrayOutputStream>();
+        for(int j = 0; j<5; j++){
+            buffers.add(new ByteArrayOutputStream());
+        }
         String line = buffer.readLine();
         line = line.replaceAll("\\r", "");
         String[] headers = line.split(",");
@@ -33,6 +51,9 @@ public class Create {
         int i = 0;
         byte[] res = {};
         while((line = buffer.readLine()) != null){
+            if(sumSizeBuffer(buffers) > 500_000_000){
+                sendChunks(buffers);
+            }
             if(res.length + line.getBytes().length < CHUNK_SIZE){
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 outputStream.write(res);
@@ -41,12 +62,12 @@ public class Create {
             }
             else{
                 byte[] newRes = {};
-                // Copie du chunk dans une variable de type final
+                buffers.get(i % 5).write(res);
                 res = newRes;
-                // Thread pour envoyer la requête...
                 i++;
             }
         } 
+        sendChunks(buffers);
         if(buffer.readLine() == null){
             System.out.println("Tout lu !");
         } 
