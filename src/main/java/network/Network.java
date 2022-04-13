76df53bcce1480f2b5d744927f79fde7b2f7936e
Sarahhabbi <habbisarah.1@gmail.com>
2022-04-13 10:56:02 +0200

    network connection

--- src/main/java/network/Network.java
+++ src/main/java/network/Network.java
@@ -1,15 +1,38 @@
 package network;
 
+import java.io.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class Network {
 
     private String ipAdress;
+    private ArrayList<String> peersIPAdressesList;
     private static Network instance;
 
     public Network(){
         this.ipAdress = getIpAddress();
+        this.peersIPAdressesList = getPeersIpAdresses();
+    }
+
+    private ArrayList<String> getPeersIpAdresses() {
+        ArrayList<String> peers = new ArrayList<>();
+
+        // file containing ip adresses of machines
+        File file = new File("./src/main/java/machine_information.txt");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String peerIpAddress;
+            // reading ip address
+            while ((peerIpAddress = br.readLine()) != null){
+                System.out.println(peerIpAddress);
+                peers.add(peerIpAddress);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return peers;
     }
 
     public static String getIpAddress() {
