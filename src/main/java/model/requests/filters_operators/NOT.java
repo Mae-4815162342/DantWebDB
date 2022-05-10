1b0d446486579a3cc3d76213d7feabe9e04f729b
Diez <62673882+zeidfazazi@users.noreply.github.com>
2022-05-10 15:28:12 +0200

    Add code to handle only Filter type for Operator classes

--- src/main/java/model/requests/filters_operators/NOT.java
+++ src/main/java/model/requests/filters_operators/NOT.java
@@ -8,28 +8,16 @@ import java.util.List;
 import model.Row;
 
 public class NOT implements Operator{
-  private HashMap<String, Object> NOT;
+  private HashMap<String, Filter> NOT;
 
   public boolean evaluate(Row row, LinkedHashMap<String, String> columns){
     List<String> columnLabel = new ArrayList<>(columns.keySet());
     List<String> values = row.toList();
     boolean res = true;
     for(String targetColumn : NOT.keySet()){
-       if(NOT.get(targetColumn).getClass() == String.class){
-          String query = (String) NOT.get(targetColumn);
-          String value = values.get(columnLabel.indexOf(targetColumn));
-          res = (!query.equals(value) && res);
-       }
-        if(NOT.get(targetColumn).getClass() == Integer.class){
-          int query = (int) NOT.get(targetColumn);
-          int value = Integer.parseInt(values.get(columnLabel.indexOf(targetColumn)));
-          res = ((query != value) && res);
-        }
-        if(NOT.get(targetColumn).getClass() == Filter.class){
           Filter queries = (Filter) NOT.get(targetColumn);
           String value = values.get(columnLabel.indexOf(targetColumn));
           res = (!queries.evaluate(value, columns.get(targetColumn)) && res);
-        }
     }
     return res;
   }
--- src/main/java/model/requests/filters_operators/OR.java
+++ src/main/java/model/requests/filters_operators/OR.java
@@ -8,28 +8,16 @@ import java.util.List;
 import model.Row;
 
 public class OR implements Operator{
-  private HashMap<String, Object> OR;
+  private HashMap<String, Filter> OR;
 
   public boolean evaluate(Row row, LinkedHashMap<String, String> columns){
     List<String> columnLabel = new ArrayList<>(columns.keySet());
     List<String> values = row.toList();
     boolean res = true;
     for(String targetColumn : OR.keySet()){
-       if(OR.get(targetColumn).getClass() == String.class){
-          String query = (String) OR.get(targetColumn);
-          String value = values.get(columnLabel.indexOf(targetColumn));
-          res = (query.equals(value) || res);
-       }
-        if(OR.get(targetColumn).getClass() == Integer.class){
-          int query = (int) OR.get(targetColumn);
-          int value = Integer.parseInt(values.get(columnLabel.indexOf(targetColumn)));
-          res = ((query == value) || res);
-        }
-        if(OR.get(targetColumn).getClass() == Filter.class){
           Filter queries = (Filter) OR.get(targetColumn);
           String value = values.get(columnLabel.indexOf(targetColumn));
-          res = (queries.evaluate(value, columns.get(targetColumn)) || res);
-        }
+          res = (!queries.evaluate(value, columns.get(targetColumn)) || res);
     }
     return res;
   }
